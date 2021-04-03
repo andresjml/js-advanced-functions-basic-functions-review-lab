2c789eeecd39a4883917aa2f770f054f7d5a4e82
Andres <andresjml@gmail.com>
2021-04-02 22:07:53 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function saturdayFun(act='roller-skate') {
+    let msg1=`This Saturday, I want to ${act}!`
+    //console.log(`This Saturday, I want to ${act}!`); 
+    return msg1;
+  }
+
+/*let fn = function mondayWork(act2="go to the office"){
+    console.log(`This Monday, I will ${act2}.`);
+}*/
+function mondayWork(act2='go to the office'){
+    let msg2=`This Monday, I will ${act2}.`
+    //console.log(`This Monday, I will ${act2}.`);
+    return msg2;
+}
+
+function wrapAdjective(wrap="*"){
+    return function innerFunct(adj="special"){
+        return `You are ${wrap}${adj}${wrap}!`
+    }
+}
+
+const Calculator = {
+    add:function(num1,num2){
+        return num1+num2;
+    },
+    subtract:function(num1,num2){
+        return num1-num2;
+    },
+    multiply:function(num1,num2){
+        return num1*num2;
+    },
+    divide:function(num1,num2){
+        return num1/num2;
+    }
+}
+
+function actionApplyer(numstart, arr){
+    let a = numstart
+    for (let i=0;i < arr.length;i++){
+        a=arr[i](a)
+    }
+    return a;
+}
